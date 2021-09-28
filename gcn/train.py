bf6940a38fd3c0ff87900553388640a1a5d68b4a
Niklas Mäki <niklas.maki@gmail.com>
2021-09-28 09:23:20 +0300

    Add MAP estimate calculation to the train algorithm

--- gcn/train.py
+++ gcn/train.py
@@ -6,7 +6,12 @@ import tensorflow as tf
 
 from gcn.utils import *
 from gcn.models import GCN, MLP
-from gcn.distances import node2vec, neighborhood_distance_matrix, weighted_distance_matrix
+from gcn.distances import node2vec_distances, neighborhood_distance_matrix, weighted_distance_matrix
+from gcn.posterior import test, map_estimate, get_allowed_edges
+
+#test()
+#exit()
+
 
 # Set random seed
 seed = 123
@@ -19,7 +24,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
 flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense'
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 200, 'Number of epochs to train.')
+flags.DEFINE_integer('epochs', 20, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
 flags.DEFINE_float('dropout', 0.5, 'Dropout rate (1 - keep probability).')
 flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
@@ -113,6 +118,9 @@ d2 = neighborhood_distance_matrix(adj, preds, FLAGS.dataset)
 
 print("Calculating weighted distance matrix...")
 d = weighted_distance_matrix(d1, d2)
+
+print("Calculating the MAP estimate...")
+map_estimate(adj, d, FLAGS.dataset)
 
 # Testing
 test_cost, test_acc, test_duration = evaluate(features, support, y_test, test_mask, placeholders)
